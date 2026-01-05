2d47b1fa7af21b8111e027bb255fb7bd3fa93ae9
Iker Vericat <ikervericat@iesmontsia.org>
2026-01-05 23:42:11 +0100

    Add: Sanctum Auth

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\StudentApiController;
 use App\Http\Controllers\API\TeacherApiController;
 use App\Http\Controllers\API\CourseApiController;
+use App\Http\Controllers\API\AuthController;
 
-Route::apiResource('students', StudentApiController::class);
-Route::post('students/{id}/assign-course', [StudentApiController::class,  'assignCourse']);
-Route::delete('students/{id}/remove-course', [StudentApiController::class, 'removeCourse']);
-Route::apiResource('courses', CourseApiController::class);
+// Rutas Públicas
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Rutas Protegidas
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('students', StudentApiController::class);
+    Route::post('students/{id}/assign-course', [StudentApiController::class,  'assignCourse']);
+    Route::delete('students/{id}/remove-course', [StudentApiController::class, 'removeCourse']);
+    Route::apiResource('courses', CourseApiController::class);
+});
