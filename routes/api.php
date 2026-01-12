3806c47280973b5f390cbc96637577e0b6c71aed
Iker Vericat <ikervericat@iesmontsia.org>
2026-01-12 20:07:31 +0100

    Fix: API requests

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\StudentApiController;
-use App\Http\Controllers\API\TeacherApiController;
 use App\Http\Controllers\API\CourseApiController;
 use App\Http\Controllers\API\AuthController;
 
-// Rutas Públicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-// Rutas Protegidas
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->name('api.')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('students', StudentApiController::class);
     Route::post('students/{id}/assign-course', [StudentApiController::class,  'assignCourse']);
